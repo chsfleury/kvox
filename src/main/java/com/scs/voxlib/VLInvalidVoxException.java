85aeb3dc475bb43d4a49f88667750841c68375d1
Charles Fleury <chs.fleury@gmail.com>
2023-05-01 22:39:55 +0200

    remove serialversionuid

--- src/main/java/com/scs/voxlib/VLInvalidVoxException.java
+++ src/main/java/com/scs/voxlib/VLInvalidVoxException.java
@@ -1,8 +1,6 @@
 package com.scs.voxlib;
 
 public class VLInvalidVoxException extends RuntimeException {
-	
-	private static final long serialVersionUID = 1L;
 
 	public VLInvalidVoxException(String message) {
         super(message);
